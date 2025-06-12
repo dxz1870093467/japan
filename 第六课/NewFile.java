9fa9751907e68c8348271670ed7d691dbc2f4838
Administrator <670391573@>
2025-06-12 12:06:50 +0900

    ｎ３ー１０００　１８５

---
+++ 第六课/NewFile.java
@@ -0,0 +1,31 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class NewFile {
+    public static void main(String[] args) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("n3-1000ことば.txt", false))) {
+            for (int i = 1; i <= 1000; i++) {
+                String fullWidthNumber = toFullWidthNumber(i);
+                writer.write(fullWidthNumber + "．");
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 将阿拉伯数字转换为全角数字
+    private static String toFullWidthNumber(int number) {
+        String numStr = String.valueOf(number);
+        StringBuilder fullWidth = new StringBuilder();
+        for (char c : numStr.toCharArray()) {
+            if (Character.isDigit(c)) {
+                fullWidth.append((char) ('０' + (c - '0')));
+            } else {
+                fullWidth.append(c);
+            }
+        }
+        return fullWidth.toString();
+    }
+}
